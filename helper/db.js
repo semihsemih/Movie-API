2c850c153c67fca14675da998de338d4f1076f3b
Semih Arslan <semihsemih@me.com>
2020-03-01 22:25:13 +0300

    db connection settings

---
+++ helper/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+module.exports = () => {
+  mongoose.connect('mongodb://localhost:27017/movie-api', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+  mongoose.connection.on('open', () => {
+    console.log('MongoDB: Connected');
+  });
+
+  mongoose.connection.on('error', err => {
+    console.log('MongoDB: Connection Error\n' , err);
+  });
+};
