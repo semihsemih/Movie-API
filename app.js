e39e5f8746340b0b61c0b7c0247f98b784a9d2a8
Semih Arslan <semihsemih@me.com>
2020-03-02 19:55:32 +0300

    Writing endpoint that lists all movies (/api/movies [GET])

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api/movie', movie);
+app.use('/api/movies', movie);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- routes/movie.js
+++ routes/movie.js
@@ -4,6 +4,18 @@ const router = express.Router();
 // Models
 const Movie = require('../models/Movie');
 
+router.get('/', (req, res) => {
+  const promise = Movie.find({});
+
+  promise
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.json(err.message);
+    });
+});
+
 router.post('/', (req, res, next) => {
   const movie = new Movie(req.body);
 
