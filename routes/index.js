8bc43c2eba69a68eefd649e088cc00e6b29188dc
semihsemih <semihsemih@me.com>
2020-03-05 16:05:18 +0300

    Token generating service created with Jsonwebtoken

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ const router = express.Router();
 // A library to help you hash passwords
 const bcrypt = require('bcryptjs');
 
+// Verify and generate JWT
+const jwt = require('jsonwebtoken');
+
 // Model
 const User = require('../models/User');
 
@@ -31,6 +34,48 @@ router.post('/register', (req, res, next) => {
         res.json(err);
       });
   });
+});
+
+router.post('/authenticate', (req, res, next) => {
+  const { username, password } = req.body;
+
+  User.findOne(
+    {
+      username
+    },
+    (err, user) => {
+      if (err) {
+        throw err;
+      }
+
+      if (!user) {
+        res.json({
+          status: false,
+          message: 'Authentication failed, user not found.'
+        });
+      } else {
+        bcrypt.compare(password, user.password).then(result => {
+          if (!result) {
+            res.json({
+              status: false,
+              message: 'Authentication failed, wrong password.'
+            });
+          } else {
+            const payload = {
+              username
+            };
+            const token = jwt.sign(payload, req.app.get('api_secret_key'), {
+              expiresIn: 720 // 12 Hours
+            });
+            res.json({
+              status: true,
+              token
+            });
+          }
+        });
+      }
+    }
+  );
 });
 
 module.exports = router;
